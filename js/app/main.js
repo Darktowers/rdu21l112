1160c72368e448223c289c74ab3f631cc8cb9b42
Cristian Arrieta <cristian-rock1@hotmail.com>
2015-08-10 16:34:52 -0500

    Implementando Require.js

---
+++ js/app/main.js
@@ -0,0 +1,7 @@
+define(["jquery"], function($) {
+    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+    $(".menu-movil").on("click",function(){
+	$(".ContenedorPrincipal-menu").show("basic");
+});
+});
+
